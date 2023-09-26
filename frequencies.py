f23f386c621bc45ce9f4828101864e237746d374
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2023-09-26 16:31:31 +0100

    Implement frequency calculator

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        stritem = str(item)
+        if stritem in frequencies.keys():
+            frequencies[stritem] = frequencies.get(stritem) + 1
+        else:
+            frequencies[stritem] = 1
+
     return frequencies
